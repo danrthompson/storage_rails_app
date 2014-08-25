4d0de039de9d2dda5cb859a06519346dc973df02
Dan Thompson <dant1@stanford.edu>
2014-08-25 12:03:04 -0700

    finished request time validation

--- app/models/request.rb
+++ app/models/request.rb
@@ -3,12 +3,17 @@ class Request < ActiveRecord::Base
 	belongs_to :user
 	belongs_to :driver, class_name: 'User'
 	validates :user_id, :delivery_time, presence: true
+	validate :delivery_time_is_available
 
 	def self.delivery_time_available?(time)
-		within_standard_times?(time) and fits_with_other_delivery_times?(time)
+		within_standard_times?(time) and fits_with_other_delivery_times?(time, all)
 	end
 
 	private
+
+	def delivery_time_is_available
+		errors.add(:delivery_time, 'is unavailable') unless Request.within_standard_times?(self.delivery_time) and Request.fits_with_other_delivery_times?(self.delivery_time, Request.where.not(id: self.id))
+	end
 
 	def self.within_standard_times?(time)
 		if time.saturday? or time.sunday?
@@ -20,7 +25,7 @@ class Request < ActiveRecord::Base
 		end
 	end
 
-	def self.fits_with_other_delivery_times?(time)
-		not where(completion_time: nil, delivery_time: (time - 1.hour)..(time + 1.hour)).exists?
+	def self.fits_with_other_delivery_times?(time, requests)
+		not requests.where(completion_time: nil, delivery_time: (time - 1.hour)..(time + 1.hour)).exists?
 	end
 end
