c38a6a0eddc4da8d264caa07a294b4d29d46812e
Dan Thompson <dant1@stanford.edu>
2014-09-11 00:09:15 -0700

    i think that solves the dashboard controller too

--- app/controllers/storage_items_controller.rb
+++ app/controllers/storage_items_controller.rb
@@ -30,11 +30,16 @@ class StorageItemsController < ApplicationController
 
 	def index
 		@user = current_user
-		@boxes_at_home = current_user.boxes_at_home
-		# @boxes_at_home_array = (0..@boxes_at_home)
-		@storage_items = StorageItem.where(user_id: current_user, left_storage_at: nil, delivery_request_id: nil).order(:item_type, :entered_storage_at)
+
+		@boxes_at_home = StorageItem.where(user_id: current_user.id, item_type: 'box', entered_storage_at: nil)
+		@medium_items_at_home = StorageItem.where(user_id: current_user.id, item_type: 'medium', entered_storage_at: nil)
+		@large_items_at_home = StorageItem.where(user_id: current_user.id, item_type: 'large', entered_storage_at: nil)
+		@extra_large_items_at_home = StorageItem.where(user_id: current_user.id, item_type: 'extra_large', entered_storage_at: nil)
+
+		@storage_items = StorageItem.where(user_id: current_user.id, left_storage_at: nil, delivery_request_id: nil).where.not(entered_storage_at: nil).order(:item_type, :entered_storage_at)
 		@num_storage_items = @storage_items.count
-		@storage_items_pending_delivery = StorageItem.where(user_id: current_user, left_storage_at: nil).where.not(delivery_request_id: nil).order(:item_type, :entered_storage_at)
+		
+		@storage_items_pending_delivery = StorageItem.where(user_id: current_user.id, left_storage_at: nil).where.not(delivery_request_id: nil, entered_storage_at: nil).order(:item_type, :entered_storage_at)
 	end
 
 
