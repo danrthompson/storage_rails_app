8cd7fb66d044287418be500913cc047ea0471440
Daniel Haarburger <work.haarburger@gmail.com>
2014-10-13 19:16:27 -0700

    adds partial for admin add entry

--- app/assets/javascripts/admin_record.js
+++ app/assets/javascripts/admin_record.js
@@ -31,8 +31,8 @@ $(".details-btn").click(function(){
 
 
 $('form').on('click', '.add_fields', function(e){
+  e.preventDefault();
   var time = new Date().getTime();
   var regexp = new RegExp($(this).data('id'), 'g');
   $(this).before($(this).data('fields').replace(regexp, time));
-  e.preventDefault();
 });
