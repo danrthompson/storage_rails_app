31767041fde19640e981c82885ea4fa836b9c584
Dan Thompson <dant1@stanford.edu>
2014-10-17 15:27:28 -0700

    now in development were using test keys and in production were using secret keys

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,3 +1,10 @@
+Rails.configuration.stripe = {
+  publishable_key: 'pk_live_K5CS0FoGhbuSqXXZXBUNdjh2',
+  secret_key: 'sk_live_EupcHOI991bPahz8vDUeTXDb'
+}
+
+Stripe.api_key = Rails.configuration.stripe[:secret_key]
+
 Rails.application.configure do
 
   # Settings specified here will take precedence over those in config/application.rb.
