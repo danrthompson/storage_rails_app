541728ac02dabfd809e291bb6b9f80ee875812d2
Daniel Haarburger <work.haarburger@gmail.com>
2014-10-19 23:48:03 -0600

    fixes validations for credit card on signup show page

--- app/assets/javascripts/cc_validations.js
+++ app/assets/javascripts/cc_validations.js
@@ -7,65 +7,72 @@
 $(document).ready(function() {
     
 
-    $(".form-control").on('input', function() {
-      $('#credit-card-form').data('bootstrapValidator').validate();
-    });
-
-    $('#credit-card-form')
-        .bootstrapValidator({
-        feedbackIcons: {
+    $('#credit-card-form').bootstrapValidator({
+      submitButtons: 'null',
+      fields: {
+        'user[cc_name]': {
+          message: 'The name field is not valid',
+          validators: {
+            notEmpty: {
+              message: 'The name field can\'t be empty'
+            }
+          }
         },
-        fields: {
-          'user[cc_name]': {
-            message: 'The name field is not valid',
-            validators: {
-              notEmpty: {
-                message: 'The name field can\'t be empty'
-              }
-            }
-          },
-          'user[cc_number]': {
-            message: 'The credit card number is not valid',
-            validators: {
-              notEmpty: {
-                message: 'credit card number can\'t be empty'
-              },
-              creditCard: {
-	              message: 'The credit card number is not valid'
-	          }
-            }
-          },
-          'user[exp_month]': {
-            message: 'expiration month not valid',
-            validators: {
-              notEmpty: {
-                message: 'expiration month required'
-              }
-            }
-          },
-          'user[exp_year]': {
-            message: 'expiration year not valid',
-            validators: {
-              notEmpty: {
-                message: 'expiration year required'
-              }
-            }
-          },
-          'user[cc_cvc]': {
-            message: 'cvc not valid',
-            validators: {
-              notEmpty: {
-                message: 'CVC required'
-              }
+        'user[cc_number]': {
+          message: 'The credit card number is not valid',
+          validators: {
+            notEmpty: {
+              message: 'credit card number can\'t be empty'
             },
-            stringLength: {
-              min: 3,
-              max: 3,
-              message: 'CVC must be 3 digits'
+            creditCard: {
+              message: 'The credit card number is not valid'
+          }
+          }
+        },
+        'user[exp_month]': {
+          message: 'expiration month not valid',
+          validators: {
+            notEmpty: {
+              message: 'expiration month required'
+            }
+          }
+        },
+        'user[exp_year]': {
+          message: 'expiration year not valid',
+          validators: {
+            notEmpty: {
+              message: 'expiration year required'
+            }
+          }
+        },
+        'user[cc_cvc]': {
+          message: 'cvc not valid',
+          validators: {
+            notEmpty: {
+              message: 'CVC required'
+            }
+          },
+          stringLength: {
+            min: 3,
+            max: 3,
+            message: 'CVC must be 3 digits'
+          }
+        },
+        'user[promo_code]': {
+          message: '',
+          validators: {
+          }
+        },
+        'user[referrer]': {
+          message: '* this field is required',
+          validators: {
+            notEmpty: {
+              message: '* this field is required'
             }
           }
         }
-      });
+      }
+    });
   });
 
 
