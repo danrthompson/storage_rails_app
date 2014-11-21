c0f20400fc4a349374aea6a2a68f6c3c925afbdc
Dan Thompson <dant1@stanford.edu>
2014-11-21 12:17:01 -0800

    homepage change

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,5 @@
 class StaticPagesController < ApplicationController
-	before_action :no_user!, only: [:homepage]
+	before_action :no_ready_users!, only: [:homepage]
 
 	def about
 		@user = current_user
@@ -23,5 +23,11 @@ class StaticPagesController < ApplicationController
 
 	def server_error
 		render status: 500
+	end
+
+	private
+
+	def no_ready_users!
+		redirect_to storage_items_url and return if current_user and current_user.ready?
 	end
 end
