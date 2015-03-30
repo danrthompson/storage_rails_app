a4a0b9cb4ee57446575ee621ef5705d4552bf7d2
Daniel Haarburger <work.haarburger@gmail.com>
2015-03-29 22:43:18 -0700

    cleans up mobile ui for storage homepage

--- db/schema.rb
+++ db/schema.rb
@@ -108,6 +108,8 @@ ActiveRecord::Schema.define(version: 20150223195852) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "first_name"
+    t.string   "last_name"
     t.string   "address_line_1"
     t.string   "address_line_2"
     t.string   "city"
